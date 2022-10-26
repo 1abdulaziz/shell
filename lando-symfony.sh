918458ff24e7e0b1e9476f0475e1f30eed47be09
abdulaziz zaid <1abdulaziz@users.noreply.github.com>
2022-10-26 15:17:41 +0300

    Create lando-symfony.sh

---
+++ lando-symfony.sh
@@ -0,0 +1,30 @@
+#/bin/sh
+
+if [ -z "$1" ];
+  then
+        exit "blablablabla"
+fi
+
+mkdir "$1" \
+  && cd "$1" \
+  && lando init \
+    --source cwd \
+    --recipe symfony \
+    --php "8.1" \
+    --via nginx \
+    --webroot public \
+    --name "$1"
+    
+    
+_type_composer_create_symfony () {
+    lando composer create-project symfony/website-skeleton -n
+    mv website-skeleton/* ./
+    mv website-skeleton/.* ./
+    rm -rf website-skeleton
+}
+
+_type_composer_create_symfony
+
+lando composer self-update
+
+lando rebuild -y
