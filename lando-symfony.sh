c47491c1896822b91c6bf7b691ce409c4d6f6c1b
abdulaziz zaid <1abdulaziz@users.noreply.github.com>
2022-11-06 12:41:12 +0300

    version symfony 6.1

--- lando-symfony.sh
+++ lando-symfony.sh
@@ -19,12 +19,11 @@ mkdir "$1" \
 lando start
 lando ssh --user root --command "apt-get update && apt-get install wget && wget https://get.symfony.com/cli/installer -O - | bash"
 lando ssh --user root --command "git config --global user.email 'aziz.sa03@gmail.com' &&   git config --global user.name 'abdulaziz zaid'"
-lando ssh --user root --command "mv /root/.symfony5/bin/symfony /usr/local/bin/symfony && cd /app && symfony new --webapp $1"
+lando ssh --user root --command "mv /root/.symfony5/bin/symfony /usr/local/bin/symfony && cd /app && symfony new $1 --version='6.1.*' --webapp"
 lando ssh --user root --command "mv /app/$1/* /app && mv /app/$1/.* /app"
 
 # old
 #_type_composer_create_symfony () {
-#    lando composer create-project symfony/website-skeleton -n
 #    mv website-skeleton/* ./
 #    mv website-skeleton/.* ./
 #    rm -rf website-skeleton
