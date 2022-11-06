c51cf560821a9293dd06cc5fd2068c04d4510bd7
abdulaziz zaid <1abdulaziz@users.noreply.github.com>
2022-11-06 11:59:28 +0300

    Update lando-symfony.sh

--- lando-symfony.sh
+++ lando-symfony.sh
@@ -14,17 +14,21 @@ mkdir "$1" \
     --via nginx \
     --webroot public \
     --name "$1"
-    
-    
-_type_composer_create_symfony () {
-    lando composer create-project symfony/website-skeleton -n
-    mv website-skeleton/* ./
-    mv website-skeleton/.* ./
-    rm -rf website-skeleton
-}
-
-_type_composer_create_symfony
+  
+# new 
+lando start
+lando ssh --user root --command "apt-get update && apt-get install wget && wget https://get.symfony.com/cli/installer -O - | bash"
+lando ssh --user root --command "git config --global user.email 'aziz.sa03@gmail.com' &&   git config --global user.name 'abdulaziz zaid'"
+lando ssh --user root --command "mv /root/.symfony5/bin/symfony /usr/local/bin/symfony && cd /app && symfony new --webapp $1"
+# old
+#_type_composer_create_symfony () {
+#    lando composer create-project symfony/website-skeleton -n
+#    mv website-skeleton/* ./
+#    mv website-skeleton/.* ./
+#    rm -rf website-skeleton
+#}
+#_type_composer_create_symfony
 
 lando composer self-update
-lando composer require mailgun-mailer
+#lando composer require mailgun-mailer
 lando rebuild -y
